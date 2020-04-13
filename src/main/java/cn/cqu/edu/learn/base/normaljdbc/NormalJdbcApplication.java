9780303432bc30c2107d4cf056eff2b355157cee
caojfc <caojfc@yonyou.com>
2020-04-13 22:59:46 +0800

    init

---
+++ src/main/java/cn/cqu/edu/learn/base/normaljdbc/NormalJdbcApplication.java
@@ -0,0 +1,13 @@
+package cn.cqu.edu.learn.base.normaljdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NormalJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NormalJdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/cqu/edu/learn/base/normaljdbc/NormalJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.cqu.edu.learn.base.normaljdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NormalJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
